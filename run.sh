45f1556e75ff4e03de946106a04dd9a19ee24640
Wil <wfkoch@gmail.com>
2015-11-15 15:16:00 -0500

    Fix OOM bug

--- run.sh
+++ run.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-#Let each analysis run for an hour
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters: [APK file list] [Output dir] [Init source/sink file]"
+fi
 
+#Let each analysis run for an hour
+APKS=$1
+OUTPUT=$2
+SINKSOURCE=$3
 LOG_LEVEL=info
-APKS=/home/wfkoch/apks/fromjson_apps-v5.txt
-OUTPUT=/data/results_fromjson
-SINKSOURCE=ss-v5.txt
 ANDROID_PLATFORMS=/home/wfkoch/libs/android-sdk-linux/platforms
-java -d64 -XX:+UseConcMarkSweepGC -Xmx160g -Xms160g -Dorg.slf4j.simpleLogger.defaultLogLevel=$LOG_LEVEL -cp .:lib/* edu.bu.android.hiddendata.FindHidden $APKS $ANDROID_PLATFORMS  --pathalgo contextsensitive --fragments --aplength 5 --SYSTIMEOUT 1800 --layoutmode none --sourcessinks $SINKSOURCE --CGALGO SPARK --output $OUTPUT
+TIMEOUT=60m
+memory=40g
+pathalgo=sourcesonly 
+
+timestamp() {
+	date +"%s"
+}
+trap "exit" INT
+while IFS='' read -r line || [[ -n $line ]]; do
+	echo "$line"
+	apkname=$(basename $line)
+	mkdir -p "$OUTPUT/$apkname"
+	log="$OUTPUT/$apkname/out-$(date +%s).log"
+	timeout -s SIGINT -k 10 $TIMEOUT /usr/bin/time -v java -d64 -XX:+UseConcMarkSweepGC -Xmx$memory -Xms$memory -Dorg.slf4j.simpleLogger.defaultLogLevel=$LOG_LEVEL -cp .:lib/* edu.bu.android.hiddendata.FindHidden $line $ANDROID_PLATFORMS  --pathalgo $pathalgo --aplength 1 --aliasflowins --layoutmode none --fragments --sourcessinks $SINKSOURCE  --output $OUTPUT  > $log 2>&1 
+	#This is a hack to make sure the java process actually is killed before continueing
+	#This solves the out of memory issue that can occur with FLowDroid
+	while ps aux  | grep $apkname | grep -v "grep" > /dev/null; do sleep 1; done
+done < "$APKS"
